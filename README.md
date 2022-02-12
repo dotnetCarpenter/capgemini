a8936b55e376ad0a334c542f3a0dadfe5c38e66e
dotnetCarpenter <jon.ronnenberg@gmail.com>
2022-02-12 14:51:03 +0100

    Setup doT with test data

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 1. [Vanilla js solution](./vanilla/dist/index.html)
 
-11:45-13:15: Logo, favicon, tailwind colors
++ 11:45-13:15: Logo, favicon, tailwind colors
++ 14:10-14-20: Learning [doT]
++ 14:20-14:45: Setup [doT] with test data
+
+
+
+[doT]: https://github.com/olado/doT
--- vanilla/js/main.mjs
+++ vanilla/js/main.mjs
@@ -1,6 +1,25 @@
 import '../style.css'
+import doT from 'dot'
 
-// document.querySelector('#app').innerHTML = `
-//   <h1 class="text-3xl font-bold underline">Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
+const data = {
+	objekter: [{
+		id: 489331915,
+		href: "https://nvdbapiles-v3.atlas.vegvesen.no/vegobjekter/103/489331915/2",
+		navn: "Fartsdemper",
+		versjon: 2,
+		startdato: "2016-03-17",
+		status: 'Registrert'
+	}, {
+		id: 489331916,
+		href: "https://nvdbapiles-v3.atlas.vegvesen.no/vegobjekter/103/489331916/1",
+		navn: "Fartsdemper",
+		versjon: 1,
+		startdato: "2014-03-29",
+		status: 'Registrert'
+	}]
+}
+
+const tmpl = document.querySelector ('#rowTmpl').textContent
+const render = doT.template (tmpl)
+
+document.querySelector ('#rows').innerHTML = render (data)
