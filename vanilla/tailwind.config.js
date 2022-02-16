4c87bfee36e9ce4741c3c6f0e86f3e759154c8f7
dotnetCarpenter <jon.ronnenberg@gmail.com>
2022-02-16 18:53:12 +0100

    removed strokeWidth from custom tailwindcss (was only used in SVG, which is now an image) and proper overwrite tailwind's defaut font-family

--- vanilla/tailwind.config.js
+++ vanilla/tailwind.config.js
@@ -2,24 +2,23 @@ const withAnimations = require('animated-tailwindcss')
 
 module.exports = withAnimations ({
   content: [
-    "./index.html",
-    "./js/**/*.{html,js,mjs}",
+    './index.html',
+    './js/**/*.{html,js,mjs}',
   ],
   theme: {
+    fontFamily: {
+      sans: '"LFT Etica", "Lucida Sans Unicode", "Lucida Grande", sans-serif',
+    },
     extend: {
       maxWidth: {
         'x-screen': '100vw'
-      },
-      strokeWidth: {
-        '1': '0.2px',
-        '2': '0.4px',
       },
       colors: {
         'svv-grey': 'hsl(201, 11%, 30%)',
         'svv-yellow': 'rgb(255, 214, 31)',
         'svv-red': 'hsl(355, 85%, 52%)'
       },
-    },
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
