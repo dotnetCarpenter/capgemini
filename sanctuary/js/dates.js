aa3da0bdc1a97e0c97c68eb92f9e2feb0eeb4d3d
dotnetCarpenter <jon.ronnenberg@gmail.com>
2022-10-27 16:02:29 +0200

    [sanctuary] introduce Vitest

--- sanctuary/js/dates.js
+++ sanctuary/js/dates.js
@@ -25,3 +25,28 @@ export {
   safeFormatDate,
   createDate
 }
+
+
+/*** Tests ***/
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest
+
+
+  it ('createDate', () => {
+    expect (createDate ('2016-03-17')).toStrictEqual (new Date ('2016-03-17'))
+    expect (createDate ()).toBe (null)
+    expect (isNaN (createDate ('not a date'))).toBe (true)
+
+    // below is a bug in V8 - the date is invalid and should produce NaN
+    expect (isNaN (createDate ('2021-02-30'))).toBe (false)
+  })
+
+  it ('format dates', () => {
+    const formatDate = safeFormatDate (formatNorwegianDate)
+    const formatDateTime = safeFormatDate (formatNorwegianDateWithTime)
+    const date = new Date ('2018-05-30T14:57:31')
+
+    expect (formatDate (date)).toBe ('30.05.2018')
+    expect (formatDateTime (date)).toBe ('30. mai 2018 kl. 14:57:31')
+  })
+}
--- sanctuary/vite.config.js
+++ sanctuary/vite.config.js
@@ -1,7 +1,12 @@
-export default {
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig ({
 	base: '/capgemini/sanctuary/',
 	build: {
 		outDir: '../docs/sanctuary',
 		emptyOutDir: true
+	},
+	test: {
+		includeSource: ['js/**/*.js'],
 	}
-}
+})
