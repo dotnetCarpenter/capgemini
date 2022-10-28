f7251503e07c76ff6fb1d7388c94194044d0e05e
dotnetCarpenter <jon.ronnenberg@gmail.com>
2022-10-28 14:21:08 +0200

    [sanctuary] convert date utils to Sanctuary

--- sanctuary/js/dates.js
+++ sanctuary/js/dates.js
@@ -1,23 +1,14 @@
+import S from 'sanctuary'
+
 const formatNorwegianDateWithTime = new Intl.DateTimeFormat ('no-NO', { dateStyle: 'long', timeStyle: 'medium' }).format
 const formatNorwegianDate = new Intl.DateTimeFormat ('no-NO', { dateStyle: 'short' }).format
 
-const safeFormatDate = formatter => date => {
-  if (date == null) return null
+//    safeFormatDate :: (a -> String) -> Maybe a -> String
+const safeFormatDate = S.maybe ('')
 
-  return formatter (date)
-}
-
-// see https://github.com/js-temporal/proposal-temporal-v2/issues/22 for a proposal to validate dateString
-const createDate = dateString => {
-  try {
-    if (dateString == null) throw new TypeError ('Invalid Date')
-
-    return new Date (dateString)
-  } catch (e) {
-      console.error (e)
-      return null
-  }
-}
+//    createDate :: a -> Maybe Date
+const createDate = S.compose (S.parseDate)
+                             (x => x == null ? '' : x)
 
 export {
   formatNorwegianDateWithTime,
@@ -31,22 +22,22 @@ export {
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
 
-
   it ('createDate', () => {
-    expect (createDate ('2016-03-17')).toStrictEqual (new Date ('2016-03-17'))
-    expect (createDate ()).toBe (null)
-    expect (isNaN (createDate ('not a date'))).toBe (true)
-
-    // below is a bug in V8 - the date is invalid and should produce NaN
-    expect (isNaN (createDate ('2021-02-30'))).toBe (false)
+    expect (createDate ('2016-03-17')).toStrictEqual (S.Just (new Date ('2016-03-17')))
+    expect (createDate (undefined)).toBe (S.Nothing)
+    expect (createDate ('not a date')).toBe (S.Nothing)
   })
 
   it ('format dates', () => {
     const formatDate = safeFormatDate (formatNorwegianDate)
     const formatDateTime = safeFormatDate (formatNorwegianDateWithTime)
-    const date = new Date ('2018-05-30T14:57:31')
+    const date = createDate ('2018-05-30T14:57:31')
+    const antiDate = createDate (undefined)
 
     expect (formatDate (date)).toBe ('30.05.2018')
+    expect (formatDate (antiDate)).toBe ('')
+
     expect (formatDateTime (date)).toBe ('30. mai 2018 kl. 14:57:31')
+    expect (formatDateTime (antiDate)).toBe ('')
   })
 }
