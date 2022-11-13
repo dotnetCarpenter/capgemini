f23b31c7e6d1989dd983d35a57dbd229ac1d7876
dotnetCarpenter <jon.ronnenberg@gmail.com>
2022-11-13 14:13:54 +0100

    [sanctuary] protect for missing HTML element to render list in

--- sanctuary/js/pages/overview.js
+++ sanctuary/js/pages/overview.js
@@ -8,9 +8,18 @@ import {
   createDate
 } from '../dates.js'
 
+//    rowTmpl :: String
 const rowTmpl = document.getElementById ('rowTmpl').textContent
+
+//    renderFunction :: Object -> String<Html>
 const renderFunction = doT.template (rowTmpl)
-const rowInsertionPoint = document.getElementById ('rows')
+
+//    rowInsertionPoint :: Maybe HtmlElement
+const rowInsertionPoint = (() => {
+  const el = document.getElementById ('rows')
+  return el ? S.Just    (el)
+            : S.Nothing
+}) ()
 
 //    formatDate :: Maybe Date -> String
 const formatDate = safeFormatDate (formatNorwegianDate)
@@ -18,7 +27,7 @@ const formatDate = safeFormatDate (formatNorwegianDate)
 //    formatDateTime :: Maybe Date -> String
 const formatDateTime = safeFormatDate (formatNorwegianDateWithTime)
 
-//    createSpeedBump :: StrMap -> StrMap
+//    createSpeedBump :: StrMap -> SpeedBump
 const createSpeedBump = data => ({
   status: 'Registrert',
   id: data.id,
@@ -45,33 +54,44 @@ const formatDates = speedBump => ({
 //    transformDataToHtmlOrError :: Object -> Either Error Html
 const transformDataToHtmlOrError = S.pipe ([
   transform,
-  S.map (S.sortBy (S.prop('startDate'))),
+  S.map (S.sortBy (S.prop ('startDate'))),
   S.map (S.map (formatDates)),
   S.map (renderFunction),
   S.maybe (S.Left (new Error ('Fant ingen fartsdempere')))
           (S.Right)
 ])
 
-//    renderSpeedBumps :: Either Error Html -> Undefined
-const renderSpeedBumps = S.either (renderError)
-                                  (html => {
-                                    rowInsertionPoint.innerHTML = html
-                                  })
-
 //    resource :: String
 const resource = 'https://nvdbapiles-v3.atlas.vegvesen.no/vegobjekter/103?kartutsnitt=270153.519,7040213.023,270332.114,7040444.864&kommune=5001&segmentering=true&inkluder=metadata'
 
+//    main :: Undefined -> Undefined
 const main = () => {
-  get ({resource, headers: {'Accept': 'application/vnd.vegvesen.nvdb-v3-rev1+json'}})
-  .pipe (S.map (transformDataToHtmlOrError))
-  .pipe (F.fork (renderError)
-                (renderSpeedBumps))
+  if (S.isJust (rowInsertionPoint)) {
+    get ({resource, headers: {'Accept': 'application/vnd.vegvesen.nvdb-v3-rev1+json'}})
+    .pipe (S.map (transformDataToHtmlOrError))
+    .pipe (F.fork (renderError)
+                  (renderSpeedBumps))
+  } else {
+    console.error ('Missing HTML tag <... id="rows" ...>')
+  }
 }
 
+
+/************ IMPURE CODE *************/
+
+function renderSpeedBumps (eitherErrorOrHtml) {
+  S.either (console.error)
+           (html => S.map (element => element.innerHTML = html)
+                          (rowInsertionPoint))
+           (eitherErrorOrHtml)
+}
+
+//       renderError :: Error -> Undefined
 function renderError (error) {
   const errorTmpl = document.getElementById ('errorTmpl').textContent
 
-  rowInsertionPoint.innerHTML = doT.template (errorTmpl) (error)
+  rowInsertionPoint.innerHTML = doT.template (errorTmpl)
+                                             (error)
 }
 
 export default {
